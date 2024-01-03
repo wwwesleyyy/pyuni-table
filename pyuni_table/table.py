827b6f8420feaf0584c1f10f62a00f13670ab5d7
Wesley Kincaid <wesley.kincaid@gmail.com>
2024-01-02 22:50:54 -0600

    expand graph machinery

--- pyuni_table/table.py
+++ pyuni_table/table.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from typing import Callable, Type, TypeVar
 
 import boto3
@@ -6,6 +6,12 @@ import boto3
 from pyuni_table.model import BaseModel
 
 T = TypeVar("T", bound=BaseModel)
+
+ItemTransitionSet = namedtuple(
+    "ItemTransition",
+    ["to_create", "to_delete", "to_update"],
+    defaults=[set(), set(), set()],
+)
 
 
 class Table:
@@ -50,6 +56,7 @@ class Table:
 
         :param field: The field to mark as unique.
         """
+
         def decorator(cls):
             self.unique_indexes[cls.__name__].add(field)
             return cls
@@ -81,14 +88,6 @@ class Table:
             ReturnItemCollectionMetrics="NONE",
         )
 
-        # DynamoDB.Client.exceptions.ConditionalCheckFailedException
-        # DynamoDB.Client.exceptions.ProvisionedThroughputExceededException
-        # DynamoDB.Client.exceptions.ResourceNotFoundException
-        # DynamoDB.Client.exceptions.ItemCollectionSizeLimitExceededException
-        # DynamoDB.Client.exceptions.TransactionConflictException
-        # DynamoDB.Client.exceptions.RequestLimitExceeded
-        # DynamoDB.Client.exceptions.InternalServerError
-
     def get(self, cls: Type[T], entity_id: str) -> T:
         """
         Get an entity from the table.
@@ -102,10 +101,78 @@ class Table:
         )
         return cls.model_validate_json(response["Item"]["data"]["S"])
 
-        # DynamoDB.Client.exceptions.ConditionalCheckFailedException
-        # DynamoDB.Client.exceptions.ProvisionedThroughputExceededException
-        # DynamoDB.Client.exceptions.ResourceNotFoundException
-        # DynamoDB.Client.exceptions.ItemCollectionSizeLimitExceededException
-        # DynamoDB.Client.exceptions.TransactionConflictException
-        # DynamoDB.Client.exceptions.RequestLimitExceeded
-        # DynamoDB.Client.exceptions.InternalServerError
+    def graph(self, entity: BaseModel) -> set:
+        """
+        Transform an entity into a graph segment of items to be saved to DynamoDB.
+
+        :param entity: The entity to transform.
+        :return: A set of items to be saved to DynamoDB.
+        """
+        root_item = {
+            "pk": {"S": str(entity.id)},
+            "sk": {"S": "!"},
+            "data": {"S": entity.model_dump_json()},
+            "model": {"S": entity.__class__.__name__},
+        }
+        unique_items = set()
+        for field in self.unique_indexes[entity.__class__.__name__]:
+            item = {
+                "pk": {
+                    "S": f"unique##{entity.__class__.__name__}.{field}##{getattr(entity, field)}"
+                },
+                "sk": {"S": "!"},
+                "model": {"S": entity.__class__.__name__},
+            }
+            unique_items.add(item)
+        return {root_item, *unique_items}
+
+    @staticmethod
+    def diff(current: set, future: set) -> ItemTransitionSet:
+        """
+        Compare two sets of items and return the difference.
+
+        :param current: The current set of items.
+        :param future: The future set of items.
+        :return: The difference between the two sets.
+        """
+        item_set = ItemTransitionSet(set(), set(), set())
+        item_set.to_create = {
+            f
+            for f in future
+            if (f["pk"], f["sk"]) not in [(c["pk"], c["sk"]) for c in current]
+        }
+        item_set.to_delete = {
+            c
+            for c in current
+            if (c["pk"], c["sk"]) not in [(f["pk"], f["sk"]) for f in future]
+        }
+        item_set.to_update = {
+            u
+            for u in future
+            if (u["pk"], u["sk"]) in [(c["pk"], c["sk"]) for c in current if c != u]
+        }
+        return item_set
+
+    def build_transactions(self, item_set: ItemTransitionSet, current: BaseModel) -> set:
+        puts = [
+            {
+                "Put": {
+                    "TableName": self.name,
+                    "Item": entity
+                }
+            }
+            for entity in item_set.to_create | item_set.to_update
+        ]
+        deletes = [
+            {
+                "Delete": {
+                    "TableName": self.name,
+                    "Key": {
+                        "pk": entity["pk"],
+                        "sk": entity["sk"],
+                    },
+                }
+            }
+            for entity in item_set.to_delete
+        ]
+        return set(puts + deletes)
